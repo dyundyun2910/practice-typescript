d0fd154b86a547659401c2ad4ce6147ba035df9d
takaki jun <dyundyun2910@yahoo.co.jp>
2022-04-29 15:26:51 +0900

    部分型の例

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
-let result: string = "";
-
-for (let i = 1; i <= 100; i++) {
-    const delimiter = !!result ? " " : "";
-
-    const fizz = (i % 3 === 0) ? "Fizz" : "";
-    const buzz = (i % 5 === 0) ? "Buzz" : "";
-
-    const fizzbuzz = fizz + buzz || i;
-    result += delimiter + fizzbuzz;
+type FooBar =  {
+    foo: string;
+    bar: number;
+}
+type FooBarBaz = {
+    foo: string;
+    bar: number;
+    baz: boolean;
 }
 
-console.log(result);
+const obj: FooBarBaz = {
+    foo: "hi",
+    bar: 1,
+    baz: false
+};
+const obj2: FooBar = obj;
