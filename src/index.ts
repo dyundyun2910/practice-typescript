bd952e2773cf84ecff0319e70b31fa2eee8b8148
takaki jun <dyundyun2910@yahoo.co.jp>
2022-04-29 17:38:02 +0900

    Map

--- src/index.ts
+++ src/index.ts
@@ -196,4 +196,10 @@
     if (result2 != null) {
         console.log(result2.groups);
     }
+
+    // Map
+    const map: Map<string, number> = new Map();
+    map.set("foo", 1234);
+    console.log(map.get("foo"));
+    console.log(map.get("bar"));
 }
