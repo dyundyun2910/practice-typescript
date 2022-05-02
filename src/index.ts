204382c6d563b0b2ebff352b2aedfc8a870c0569
takaki jun <dyundyun2910@yahoo.co.jp>
2022-05-02 17:33:13 +0900

    3.8力試し

--- src/index.ts
+++ src/index.ts
@@ -202,4 +202,39 @@
     map.set("foo", 1234);
     console.log(map.get("foo"));
     console.log(map.get("bar"));
+
+    // 3.8 力試し
+    {
+        type User = {
+            name: string;
+            age: number;
+            premiumUser: boolean;
+        }
+
+        const data: string = `
+        uhyo,26,1
+        John Smith,17,0
+        Mary Sue,14,1
+        `;
+
+        const users: Array<User> = [];
+        
+        const lines = data.split("\n");
+        for (const line of lines) {
+            if (line === "") continue;
+            const [name, ageString, premiumUserString] = line.split(",");
+            const age = Number(ageString);
+            const premiumUser = premiumUserString === "1";
+            
+            users.push({name, age, premiumUser});
+        }
+
+        for (const user of users) {
+            if (user.premiumUser) {
+                console.log(`${user.name} (${user.age})はプレミアムユーザです。`)
+            } else {
+                console.log(`${user.name} (${user.age})はプレミアムユーザではありません。`)
+            }
+        }
+    }
 }
